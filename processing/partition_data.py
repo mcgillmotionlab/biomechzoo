56278f4ebcc2ff9a194d6663d1920b44dfb1771b
Phil Dixon <mcgillmotionlab@gmail.com>
2025-06-27 13:01:34 +0100

    update partition

--- processing/partition_data.py
+++ processing/partition_data.py
@@ -1,12 +1,35 @@
+from utils.findfield import findfield
+import warnings
+
 def partition_data(data, evt_start, evt_end):
+    """ partition data for all channels between events evt_start and evt_end"""
+
+    # extract event values
+    e1, _ = findfield(data,evt_start)
+    e2, _ = findfield(data,evt_end)
+
     data_new = data.copy()
     for ch_name, ch_data in data_new.items():
         if ch_name != 'zoosystem':
-            ch_data_line = _partition_line(ch_data['line'], evt_start, evt_end)
-            ch_data_events = _partition_event(ch_data['event'], evt_start, evt_end)
-            ch_data_normalized = normalize_line(ch_data_line, nlength)
-    data_new[ch_name]['line'] = ch_data_normalized
-    data_new[ch_name]['event'] = ch_data_event
+            try:
+                data_new[ch_name]['line'] = ch_data[e1[0]:e2[0], :]
+            except (IndexError, ValueError) as e:
+                # IndexError: if e1[0]:e2[0] goes beyond the available indices
+                # ValueError: less likely, but may arise with shape mismatches
+                warnings.warn(f"Skipping {ch_name} due to error: {e}")
+
+            # partition events
+            events = ch_data['event']
+            for event_name, value in events.items():
+                original_frame = value[0]
+                if original_frame == 1:
+                    continue  # leave index 1 as is (per MATLAB version)
+                elif original_frame == 999:
+                    continue  # do not change outlier markers
+                else:
+                    new_frame = original_frame - e1[0] + 1
+                    data_new[ch_name]['event'][event_name][0] = new_frame
+
     return data_new
 
 
