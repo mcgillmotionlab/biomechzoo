deca439158145f65c803ff40553f5b6d60793589
Phil Dixon <mcgillmotionlab@gmail.com>
2025-07-14 15:18:36 -0400

    update filter code

--- biomech_ops/filter_data.py
+++ biomech_ops/filter_data.py
@@ -1,20 +1,42 @@
+import numpy as np
 from filter_line import filter_line
 
 
 def filter_data(data, ch, filt=None):
-    """ filter all channels in ch according to filt properties
-    Arguments:
-        data: dict, loaded zoo file
-        ch: list, channels to filter
-        filt: dict, contains filter instructions
     """
-    # raise NotImplementedError
-    # # set filter type
+    Filter one or more channels from a zoo data dictionary using specified filter parameters.
 
-    for channel in ch:
-            ch_data_line = data[channel]['line']
-            data[channel]['line'] = filter_line(ch_data_line, filt)
+    Arguments
+    ----------
+    data : dict
+        The zoo data dictionary containing signal channels.
+    ch : str or list of str
+        The name(s) of the channel(s) to filter.
+    filt : dict, optional
+        Dictionary specifying filter parameters. Keys may include:
+        - 'type': 'butter' (default)
+        - 'order': filter order (default: 4)
+        - 'cutoff': cutoff frequency or tuple (Hz)
+        - 'btype': 'low', 'high', 'bandpass', 'bandstop' (default: 'low')
+
+    Returns
+    -------
+    dict
+        The updated data dictionary with filtered channels.
+    """
+
+    if filt is None:
+        filt = {}
+
+    if isinstance(ch, str):
+        ch = [ch]
+
+    for c in ch:
+        if c not in data:
+            raise KeyError('Channel {} not found in data'.format(c))
+
+        signal_raw = data[c]['line']
+        signal_filtered = filter_line(signal_raw, filt)
+        data[c]['line'] = signal_filtered
 
     return data
-
-
--- biomech_ops/filter_line.py
+++ biomech_ops/filter_line.py
@@ -1,13 +1,56 @@
-def filter_line(r, filt):
-    """ filters data in array r according to filt
-    Args:
-        r: nd array, original array
-        filt: dict, filter settings
+import numpy as np
+from scipy.signal import butter, filtfilt
 
-    Returns:
-        r_filt: nd array, filtered array
-    """
-    raise NotImplementedError
+
+def filter_line(signal_raw, filt):
+    """ filter an array
+
+    Arguments
+    ----------
+    signal_raw : n, or n x 3 array signal to be filtered
+    filt : dict, optional
+        Dictionary specifying filter parameters. Keys may include:
+        - 'type': 'butter' (default)
+        - 'order': filter order (default: 4)
+        - 'cutoff': cutoff frequency or tuple (Hz)
+        - 'btype': 'low', 'high', 'bandpass', 'bandstop' (default: 'low')
+
+    Returns
+    -------
+    signal_filtered: filtered version of signal_raw"""
+
+    if filt is None:
+        filt = {}
+
+    # Set default filter parameters
+    ftype = filt.get('type', 'butter')
+    order = filt.get('order', 4)
+    cutoff = filt.get('cutoff', None)
+    btype = filt.get('btype', 'low')
+    fs = filt.get('fs', None)
+
+    if ftype != 'butter':
+        raise NotImplementedError(f"Filter type '{ftype}' not implemented.")
+
+    if fs is None:
+        raise ValueError("Sampling frequency 'fs' must be specified in filt.")
+
+    if cutoff is None:
+        raise ValueError("Cutoff frequency 'cutoff' must be specified in filt.")
+
+    nyq = 0.5 * fs
+    norm_cutoff = np.array(cutoff) / nyq
+
+    b, a = butter(order, norm_cutoff, btype=btype, analog=False)
+
+    if signal_raw.ndim == 1:
+        signal_filtered = filtfilt(b, a, signal_raw)
+    else:
+        # Apply filter to each column if multivariate
+        signal_filtered = np.array([filtfilt(b, a, signal_raw[:, i]) for i in range(signal_raw.shape[1])]).T
+
+    return signal_filtered
+
 
 
 
