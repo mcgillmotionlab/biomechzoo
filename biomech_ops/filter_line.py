4eb7e6a186deffb6cd804ec29cfa9aebc7f8edde
Phil Dixon <mcgillmotionlab@gmail.com>
2025-07-14 15:33:35 -0400

    add check for filter_line.py

--- biomech_ops/filter_line.py
+++ biomech_ops/filter_line.py
@@ -14,14 +14,16 @@ def filter_line(signal_raw, filt):
         - 'order': filter order (default: 4)
         - 'cutoff': cutoff frequency or tuple (Hz)
         - 'btype': 'low', 'high', 'bandpass', 'bandstop' (default: 'low')
+        - 'fs' frequency
 
     Returns
     -------
     signal_filtered: filtered version of signal_raw"""
-
+    # todo allow for missing frequency to be obtained from zoosystem metadata
     if filt is None:
         filt = {}
-
+    if filt['type'] is 'butterworth':
+        filt['type'] = 'butter'
     # Set default filter parameters
     ftype = filt.get('type', 'butter')
     order = filt.get('order', 4)
@@ -52,5 +54,35 @@ def filter_line(signal_raw, filt):
     return signal_filtered
 
 
+if __name__ == '__main__':
+    """ -------TESTING--------"""
+    import os
+    import matplotlib.pyplot as plt
+    from utils.zload import zload
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(current_dir)
+    fl = os.path.join(project_root, 'data', 'other', 'HC030A05.zoo')
+    data = zload(fl)
+    data = data['data']
+    signal_raw = data['ForceFz1']['line']
+    filt = {'type': 'butterworth',
+            'order': 3,
+            'cutoff': 20,
+            'btype': 'low',
+            'fs': data['zoosystem']['Analog']['Freq']
+            }
+    signal_filtered = filter_line(signal_raw, filt)
+
+    # now plot
+    plt.figure(figsize=(10, 4))
+    plt.plot(signal_raw, label='Raw', alpha=0.6)
+    plt.plot(signal_filtered, label='Filtered', linewidth=2)
+    plt.xlabel('Frame')
+    plt.ylabel('Amplitude')
+    plt.title('Testing filter_line')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 
