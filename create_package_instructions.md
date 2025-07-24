0c8b91be04b0781ff4efc7120655a3ce03a9fd9f
Phil Dixon <mcgillmotionlab@gmail.com>
2025-07-24 14:21:34 -0400

    update for package

---
+++ create_package_instructions.md
@@ -0,0 +1,14 @@
+# make a test python 3.10 env
+conda create -n test-bmz python=3.10 -c conda-forge
+conda activate test-bmz
+
+# building
+cd to biomechzoo-conda where the recipe files are
+conda build recipe -c conda-forge
+conda install --use-local biomechzoo -c conda-forge
+
+# test installation
+cd to repo root
+python
+import biomechzoo
+print(biomechzoo.__version__)  
--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='biomechzoo',
-    version='0.1.0',
+    name="biomechzoo",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'ezc3d'
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "ezc3d",
+        "pandas",
+        "mvnx",
     ],
-    author='Your Name',
-    description='A Python toolbox for biomechanical signal analysis',
-    url='https://github.com/mcgillmotionlab/biomechzoo',
-    license='MIT',
 )
