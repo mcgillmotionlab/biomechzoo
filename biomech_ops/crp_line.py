a838e07d3bb1494e7edab58f09001f69e388c6cc
KaiWenC98 <kai.chien@mail.mcgill.ca>
2025-07-27 18:10:03 +0800

    crp_line added

--- biomech_ops/crp_line.py
+++ biomech_ops/crp_line.py
@@ -1,6 +1,12 @@
 def crp_line(dist, prox):
     """ kai's crp code"""
-
-
-
+    """ This function determines the CRP on a 0-180 scale, correcting for
+       discontinuity in the signals >180.
+       """
+    temp_CRP = abs(dist - prox)
+    idx = temp_CRP > 180  # This corrects discontinuity in the data and puts everything on a 0-180 scale.
+    temp_CRP[idx] = 360 - temp_CRP[idx]
+    crp = temp_CRP
     return crp
+
+
