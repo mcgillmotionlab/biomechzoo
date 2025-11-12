eace6738506d94379c71a4baf9e46ede6d3050d7
jillemmerzaal <100705379+jillemmerzaal@users.noreply.github.com>
2025-11-12 15:47:59 -0500

    Ensembler version 0.0.1 update

--- examples/test_visualization.py
+++ examples/test_visualization.py
@@ -4,18 +4,19 @@ from biomechzoo import biomechzoo
 
 # get raw data folder
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-fld_data = os.path.join(project_root, 'data', 'sample_study', 'raw c3d files')
+fld_data = os.path.join(project_root, 'data', 'sample_study', 'opencap files')
 
 
-# set up a new 'bmech' object for processing
+#%% set up a new 'bmech' object for processing
 bmech = BiomechZoo(fld_data, inplace=False, verbose='all')
 
-# Step 1: Conversion to the biomechZoo format -------------------------------------------
-bmech.c3d2zoo(out_folder='visualization')
+#%% Step 1: Conversion to the biomechZoo format -------------------------------------------
+# bmech.c3d2zoo(out_folder='visualization c3d')
+bmech.table2zoo(extension='csv', out_folder='visualization opencap')
 bmech.inplace = True
 
-# Step 2: Normalize-------
-bmech.normalize()
+#%% Step 2: Normalize-------
+# bmech.normalize()
 
 # Step 3: bmech explode channel
 bmech.explodechannel()
@@ -23,7 +24,15 @@ bmech.explodechannel()
 # ---- test visualisation in script----
 from biomechzoo.visualization.ensembler import Ensembler
 
-ensembler = Ensembler(fld=bmech.in_folder, ch=['RHipAngles_x', 'RKneeAngles_x', 'RAnkleAngles_x'], conditions=['Straight', 'Turn'])
+ensembler = Ensembler(fld=bmech.in_folder, ch=['hip_flexion_r', 'knee_angle_r', 'ankle_angle_r'],
+                      conditions=['Pre', 'Post'],
+                      name_contains=["jogging"],
+                      side="_r")
 ensembler.cycles()
+# TODO: priority add markers/event
+# TODO: priority super-impose the pre-Post/turn-straight whatever conditions there are.
+# TODO: Test ensembler on Charles' data
+# TODO: is there a way to hide the cycles. without the legend?
+# TODO: find method to deal with outliers.
 ensembler.average()
 
--- src/biomechzoo/visualization/ensembler.py
+++ src/biomechzoo/visualization/ensembler.py
@@ -10,15 +10,32 @@ from biomechzoo.utils.zload import zload
 
 
 class Ensembler:
-    def __init__(self, fld, ch, conditions):
+    def __init__(self, fld, ch, conditions, name_contains=None, side=None, show_legend=False):
         self.fld = fld
         self.conditions = conditions
         self.channels = ch
-        self.zoo_files = engine(fld, extension=".zoo", subfolders=conditions)
+        self.show_legend = show_legend
+        self.zoo_files = engine(fld, extension=".zoo", subfolders=conditions, name_contains=name_contains)
         self.fig = self._create_subplots()
+        if side is not None:
+            self.side = side
+            self._filter_side_from_path()
 
     def _assign_colors(self):
         NotImplementedError()
+
+
+    def  _assign_condition_colors(self, i):
+        hex_code = pc.qualitative.D3[i]
+        h = hex_code.lstrip('#')
+        RGB =tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
+        opacity = (0.3,)
+        rgba = RGB + opacity
+
+        line_color = hex_code
+        shade_color = f"rgba{rgba}"
+
+        return line_color, shade_color
 
 
     def _create_subplots(self):
@@ -35,7 +52,16 @@ class Ensembler:
                 return cond
         return "Unknown"
 
+    def _filter_side_from_path(self):
+        self.zoo_files = [zoo_file for zoo_file in self.zoo_files if self.side in zoo_file]
+
+
     def cycles(self):
+        # check if fig is populated
+        if self.fig.data:
+            self.fig.data = []
+
+        # loop thought the zoofiles and plot the traces
         for fl in self.zoo_files:
             data = zload(fl)
             fname = os.path.basename(fl)
@@ -51,6 +77,10 @@ class Ensembler:
 
 
     def average(self):
+        # check if fig is populated
+        if self.fig.data:
+            self.fig.data = []
+
         # Initialize dictionary to store data
         data_new = {c: {ch: [] for ch in self.channels} for c in self.conditions}
 
@@ -67,7 +97,9 @@ class Ensembler:
                     print(f"Channel {channel} not found in file {fl}")
 
         # Average per condition per channel
-        for condition in data_new:
+
+        for c, condition in enumerate(data_new):
+            line_color, shade_color = self._assign_condition_colors(c)
             for i, channel in enumerate(data_new[condition]):
                 line_data = data_new[condition][channel]
                 array_data = np.array(line_data)
@@ -77,11 +109,10 @@ class Ensembler:
                 # populate the figure
                 row = i + 1
                 col = self.conditions.index(condition) + 1
-                self.add_line(y=average, row=row, col=col, name=f"{condition} - {channel}", color='#1F77B4')
-                self.add_errorbar(y=average, yerr=standard_dev, row=row, col=col, color="rgba(31,119,180,0.3)")
+                self.add_line(y=average, row=row, col=col, name=f"{condition} - {channel}", color=line_color) # color='#1F77B4')
+                self.add_errorbar(y=average, yerr=standard_dev, row=row, col=col, color = shade_color) #="rgba(31,119,180,0.3)")
 
         self.show()
-
 
 
     def add_line(self, y, x=None, row=1, col=1, name=None, color=None):
@@ -109,5 +140,6 @@ class Ensembler:
 
     def show(self):
         self.fig.update_layout(height=300 * len(self.channels), width=400 * len(self.conditions),
-                               template="simple_white",)
+                               template="simple_white",
+                               showlegend=self.show_legend)
         self.fig.show()
