30b5b069911463cbc979c8804266d08e7be3dc55
Phil Dixon <PhilD001@users.noreply.github.com>
2025-11-16 11:37:03 -0500

    update for v0.5.10

--- src/biomechzoo/processing/addevent_data.py
+++ src/biomechzoo/processing/addevent_data.py
@@ -30,7 +30,7 @@ def addevent_data(data, channels, ename, etype, constant=None):
             fsamp = data['zoosystem']['Analog']['Freq']
         else:
             raise ValueError('Cannot extract sampling frequency associated with data')
-        
+
         yd = data_new[channel]['line']  # 1D array
         etype = etype.lower()
         if etype == 'absmax':
