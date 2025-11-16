18a101cc06ae662d4b7a9889250972cb8332c004
Phil Dixon <PhilD001@users.noreply.github.com>
2025-11-16 11:33:33 -0500

    bug fix addevent_data and movement_onset

--- src/biomechzoo/biomech_ops/movement_onset.py
+++ src/biomechzoo/biomech_ops/movement_onset.py
@@ -37,10 +37,28 @@ def movement_onset(yd, fsamp, constants):
 
 
 def movement_offset(yd, fsamp, constant):
+    raise NotImplementedError
     # ----extract the constants----
-    fs = fsamp
-    mean_thresh = constant[0]
-    std_thresh = constant[1]
+    fs = constant[0]
+    mean_thresh = constant[1]
+    std_thresh = constant[2]
+    min_thresh = 0.1
+    onset_time = None
+
+    # ----Reverse, filter and extract features from the signa;----
+    acc_mag = yd.copy()
+    acc_mag = acc_mag[::-1]
+
+    acc_mag_filtered = bw_filter(data=acc_mag, N=4, fc=20, btype="low", fsamp=fsamp)
+    features, timestamps = sliding_window_features(ch_data=acc_mag_filtered, fs=fsamp)
+
+    # ----reverse timestamps----
+    timestamps = timestamps[::-1]
+
+    while onset_time is None and mean_thresh > min_thresh:
+        # ----Check if already moving----
+        if check_already_moving(features=features, mean_thresh=mean_thresh, std_thresh=std_thresh):
+            onset_time = timestamps[0]
 
         # ----Try detecting onset----
         else:
