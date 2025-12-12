bed8c03444ce6b2b29a8de31430d430753713930
jillemmerzaal <100705379+jillemmerzaal@users.noreply.github.com>
2025-12-12 16:06:48 -0500

    update ensembler: qc_app.py ---in progress

--- examples/qc_example.py
+++ examples/qc_example.py
@@ -3,12 +3,12 @@ import os
 from biomechzoo.visualization.qc_app import run_quality_check
 
 
-root= os.getcwd()
-fld = "/Users/Werk/Documents/Postdoc-McGill/breast-reduction/data/normalize"
-out_folder = os.path.join(root, 'outlier_report')
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+fld_data = os.path.join(project_root, 'data', 'csv', 'opencap')
 
 subj_pattern = [r"\b\d{3}[A-Z]{2}\b", r"\b\d{3}[A-Z]{3}\b"]
+ch = ['hip_flexion_r']
 
-run_quality_check(fld=fld, ch="gy_shank", out_folder=out_folder, conditions="pre", name_contains=["Walking"],
+run_quality_check(fld=fld_data, ch=ch, out_folder=None, conditions=["Pre", "Post"], name_contains=["jogging", '_r.'],
                   subj_pattern=subj_pattern)
 
