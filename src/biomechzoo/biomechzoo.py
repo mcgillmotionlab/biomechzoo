73f1ffcce0a24f568eda2feb37cee58e5527c77a
Phil Dixon <PhilD001@users.noreply.github.com>
2026-05-14 16:18:57 -0400

    update combine_files

--- src/biomechzoo/biomechzoo.py
+++ src/biomechzoo/biomechzoo.py
@@ -193,6 +193,9 @@ class BiomechZoo:
         if inplace is None:
             inplace = self.inplace
 
+        if out_folder is None:
+            out_folder = fld2
+
         if within:
             combine_files_within(fld=in_folder, suffix_map=suffix, name_contains=self.name_contains, subfolders=self.subfolders, inplace=inplace, out_folder=out_folder,)
         else:
@@ -201,6 +204,11 @@ class BiomechZoo:
                                   method=method, inplace=inplace,
                                   fl1exclude=fl1exlude, fl2exclude=fl2exclude,
                                   out_folder=out_folder, strmatch=strmatch)
+
+        method_name = inspect.currentframe().f_code.co_name
+        batchdisp(
+            '{} process complete for in {:.2f} secs'.format(method_name, time.time() - start_time),
+            level=1, verbose=verbose)
 
         self._update_folder(out_folder, inplace, in_folder)
 
